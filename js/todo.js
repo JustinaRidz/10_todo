bd3ff60c6f3e1d8f922b7ae3e114875a750bc74f
Justina <justina.ridz@gmail.com>
2020-05-12 23:05:30 +0300

    Naujo darbo registravimo forma #4

--- js/todo.js
+++ js/todo.js
@@ -70,6 +70,8 @@ const DOMformActions = DOMform.querySelector('.actions');
 const DOMformAdd = DOMformActions.querySelector('.btn.add');
 const DOMformClear = DOMformActions.querySelector('.btn.clear');
 
+DOMdeadlineInput.value = formatedDate(86400000);
+
 DOMformClear.addEventListener('click', clearForm);
 
 function clearForm() {
@@ -79,27 +81,54 @@ function clearForm() {
 DOMformAdd.addEventListener('click', addNewTodoItem);
 
 function addNewTodoItem() {
-  const now = new Date();
-  let mintutes = now.getMinutes();
-  let hours = now.getHours();
-  const days = now.getDate();
-  const month = now.getMonth() +1;
-  const year = now.getFullYear;
+    let newTodo = {
+        description: DOMtaskTextarea.value.trim(),
+        created_on: formatedDate(),
+        deadline: DOMdeadlineInput.value.trim(),
+        status: 'todo'
+    };
 
-  if ( minutes < 10 ) {
-    minutes = '0'+ minutes;
-  }
-  if (hours < 10) {
-    hours = '0'+ hours;
-  }
-  if (days < 10 ){
-    days = '0'+ days;
-  }
-  if (month< 10) {
-    month = '0'+ month;
-  }
-   
-  const timeNow = year+ '-' +month+ ' ' +hours+ ':' +mintutes;
+    if ( newTodo.description.length === 0 ) {
+        return alert('ERROR: tuscias aprasymas');
+    }
+
+    if ( newTodo.deadline.length > 0 &&
+        (new Date(newTodo.deadline)).toString() === 'Invalid Date' ) {
+        return alert('ERROR: nevalidus deadline');
+    }
+
+    todo_list.push( newTodo );
+
+    return;
+}
+
+function formatedDate( deltaTime = 0 ) {
+    let now = new Date();
+
+    if ( deltaTime !== 0 ) {
+        now = new Date( Date.now() + deltaTime );
+    }
+
+    let minutes = now.getMinutes();
+    let hours = now.getHours();
+    let days = now.getDate();
+    let month = now.getMonth() + 1;
+    const year = now.getFullYear();
+
+    if ( minutes < 10 ) {
+        minutes = '0'+minutes;
+    }
+    if ( hours < 10 ) {
+        hours = '0'+hours;
+    }
+    if ( days < 10 ) {
+        days = '0'+days;
+    }
+    if ( month < 10 ) {
+        month = '0'+month;
+    }
+
+    return year+'-'+month+'-'+days+' '+hours+':'+minutes;
 
 
   let newTodo = {
