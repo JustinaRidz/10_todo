2b7d3b954bf877a0af3cd5d44c0db127ae5f6fac
Justina <justina.ridz@gmail.com>
2020-05-17 18:18:03 +0300

    Pasirinkto TODO darbo redagavimas #6

--- js/todo_second.js
+++ js/todo_second.js
@@ -28,8 +28,7 @@ function renderList(list){
 function renderTodoItem(data){
     const id = 'todo_' + data.id;
     const HTML = `
-        <div class="item">
-        <div class="item" id="${id}">
+        <div class="item" id="${id}" data-task-id="${data.id}">
             <div class="status ${data.status}"></div>
             <p class="description">${data.description}</p>
             <div class="deadline">${data.deadline}</div>
@@ -46,12 +45,12 @@ function renderTodoItem(data){
 
     // registruojame event listener'i
     item.querySelector('.action.remove')
-        .addEventListener('click', () => {
+        .addEventListener('click', () =>{
             let currentlyAddedItemIndex = 0;
 
             // randu kelintas siuo metu sarase yra norimas mano TODO item
-            for (let i = 0; i < DOMitems.length; i++) {
-                if (DOMitems[i].id === id) {
+            for (let i = 0; i < DOMitems.length; i++){
+                if (DOMitems[i].id === id){
                     currentlyAddedItemIndex = i;
                     break;
                 }
@@ -59,11 +58,30 @@ function renderTodoItem(data){
 
             removeTodo(currentlyAddedItemIndex);
         });
+
     item.querySelector('.action.edit')
-        .addEventListener('click', () => {
+        .addEventListener('click', () =>{
+            DOMcontainer.classList.add('editing');
             DOMform.classList.add('editing');
+            populateEditingForm(data.id);
         });
     return;
+}
+
+function populateEditingForm(id){
+    let task = {};
+    let i = 0;
+    for (; i < todo_list.length; i++){
+        if (todo_list[i].id === id){
+            task = todo_list[i];
+            break;
+        }
+    }
+
+    DOMform.setAttribute('data-task-index', i);
+    DOMtaskTextarea.value = task.description;
+    DOMdeadlineInput.value = task.deadline;
+    DOMswitchStatus.setAttribute('data-selected', task.status);
 }
 
 function formatedDate(deltaTime = 0){
@@ -79,7 +97,7 @@ function formatedDate(deltaTime = 0){
     let month = now.getMonth() + 1;
     const year = now.getFullYear();
 
-    if (minutes < 10) {
+    if (minutes < 10){
         minutes = '0' + minutes;
     }
     if (hours < 10){
@@ -88,11 +106,11 @@ function formatedDate(deltaTime = 0){
     if (days < 10){
         days = '0' + days;
     }
-    if (month < 10){
+    if (month < 10) {
         month = '0' + month;
     }
 
-    return year + '-' + month + '-' + days + ' ' + hours + ':' + minutes;
+    return year+ '-' +month+ '-' +days+ ' ' +hours+ ':' +minutes;
 }
 
 function removeAllTodos(){
@@ -108,32 +126,32 @@ function removeTodo(todoIndex){
 
     // remove item from todo_list (global variable)
     let leftTodos = [];
-    for (let i = 0; i < todo_list.length; i++){
+    for (let i = 0; i < todo_list.length; i++) {
         if (i !== todoIndex) {
             leftTodos.push(todo_list[i]);
         }
     }
+
     todo_list = leftTodos;
     updateMemory();
     return;
 }
 
-function createNewTodo() {
-    todo_id++;
+function createNewTodo(){
     let newTodo = {
         id: todo_id,
-        description: DOMtaskTextarea.value.trim(),
         created_on: formatedDate(),
+        description: DOMtaskTextarea.value.trim(),
         deadline: DOMdeadlineInput.value.trim(),
         status: 'todo'
     };
 
-    if (newTodo.description.length === 0) {
+    if (newTodo.description.length === 0){
         return alert('ERROR: tuscias aprasymas');
     }
 
     if (newTodo.deadline.length > 0 &&
-        (new Date(newTodo.deadline)).toString() === 'Invalid Date') {
+        (new Date(newTodo.deadline)).toString() === 'Invalid Date'){
         return alert('ERROR: nevalidus deadline');
     }
 
@@ -142,14 +160,56 @@ function createNewTodo() {
     todo_id++;
     updateMemory();
 }
-function updateSwitch(event) {
+
+function updateTaskInfo(){
+    const index = parseInt(DOMform.dataset.taskIndex);
+    const description = DOMtaskTextarea.value;
+    const deadline = DOMdeadlineInput.value;
+    const status = DOMswitchStatus.dataset.selected;
+
+    if (description.length === 0){
+        return alert('ERROR: tuscias aprasymas');
+    }
+
+    if (deadline.length > 0 &&
+        (new Date(deadline)).toString() === 'Invalid Date'){
+        return alert('ERROR: nevalidus deadline');
+    }
+
+    todo_list[index].description = description;
+    todo_list[index].deadline = deadline;
+    todo_list[index].status = status;
+
+    updateMemory();
+
+    // update HTML
+    const task_id = '#todo_' + todo_list[index].id;
+    const DOMtask = DOMcontainer.querySelector(task_id);
+    DOMtask.querySelector('.description').innerText = description;
+    DOMtask.querySelector('.deadline').innerText = deadline;
+    const DOMstatus = DOMtask.querySelector('.status');
+    DOMstatus.classList.remove('todo', 'progress', 'done');
+    DOMstatus.classList.add(status);
+}
+
+function clearForm(){
+    DOMtaskTextarea.value = '';
+    DOMswitchStatus.setAttribute('data-selected', 'todo');
+    DOMdeadlineInput.value = formatedDate(86400000);
+    DOMcontainer.classList.remove('editing');
+    DOMform.classList.remove('editing');
+}
+
+function updateSwitch(event){
     const value = event.target.dataset.option;
     event.target.parentElement.setAttribute('data-selected', value);
 }
 
-    //MEMORY MANAGEMENT
+/*******************************
+    MEMORY MANAGEMENT
+*******************************/
 
-function memoryManagement() {
+function memoryManagement(){
     if (localStorage.getItem('todo_id')){
         // jei yra, tai is localStorage istraukiu esama reiksme ir ja priskiriu todo_id
         todo_id = JSON.parse(localStorage.getItem('todo_id'));
@@ -168,24 +228,37 @@ function memoryManagement() {
     }
 }
 
-function updateMemory() {
+function updateMemory(){
     localStorage.setItem('todo_id', JSON.stringify(todo_id));
     localStorage.setItem('todo_list', JSON.stringify(todo_list));
 }
+
 memoryManagement();
+
 
     //GENERATE CONTENT
 renderList(todo_list);
+
 DOMdeadlineInput.value = formatedDate(86400000);
 
 
-    //INIT ACTIONS
+   // INIT ACTIONS
 BTNremoveAll.addEventListener('click', removeAllTodos);
 
-DOMformAdd.addEventListener('click', createNewTodo);
+DOMformAdd.addEventListener('click', () =>{
+    createNewTodo();
+    clearForm();
+});
+
+DOMformClear.addEventListener('click', clearForm);
 
 DOMswitchStatus.addEventListener('click', updateSwitch);
 
-DOMformCancel.addEventListener('click', () => {
-    DOMform.classList.remove('editing');
-})
+DOMformSave.addEventListener('click', () =>{
+    updateTaskInfo();
+    clearForm();
+});
+
+DOMformCancel.addEventListener('click', () =>{
+    clearForm();
+});
